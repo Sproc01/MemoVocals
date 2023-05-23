229a5da97bd320504a8ae60a4a414d41ddccf743
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2023-05-23 14:33:22 +0200

    remove useless timer

--- app/src/main/java/com/example/memovocali/PlayerService.kt
+++ app/src/main/java/com/example/memovocali/PlayerService.kt
@@ -20,8 +20,6 @@ class PlayerService: Service() {
     private lateinit var title:String
     private var duration: Int=0
     private var seek:Int=0
-    private var timer: CountDownTimer?=null
-    private var notification: Notification? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -40,34 +38,6 @@ class PlayerService: Service() {
             myPlayer?.pause()
             myPlayer?.seekTo(seek)
             myPlayer?.start()
-            timer?.cancel()
-            timer=object: CountDownTimer(duration.toLong()-seek, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                   /* if (ActivityCompat.checkSelfPermission(
-                            applicationContext,
-                            Manifest.permission.POST_NOTIFICATIONS
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        return
-                    }
-                    val notificationBuilder: Notification.Builder =
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                            Notification.Builder(applicationContext, CHANNEL_ID)
-                        else  // Deprecation warning left on purpose for educational reasons
-                            Notification.Builder(applicationContext)
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        notificationBuilder.setBadgeIconType(Notification.BADGE_ICON_SMALL)
-                    }
-                    notificationBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
-                    notificationBuilder.setContentTitle(title)
-                    notificationBuilder.setProgress(duration,(duration-millisUntilFinished).toInt(),false)
-                    notification = notificationBuilder.build()
-                    NotificationManagerCompat.from(applicationContext).notify(5786423, notification!!)*/
-                }
-                override fun onFinish() {
-                    stop()
-                }
-            }
         }
         return START_STICKY
     }
@@ -108,15 +78,6 @@ class PlayerService: Service() {
         // or stopped
         myPlayer!!.setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
         myPlayer?.start()
-        timer=object: CountDownTimer(duration.toLong(), 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                if(myPlayer!=null)
-                    seek=myPlayer!!.currentPosition
-            }
-            override fun onFinish() {
-                stop()
-            }
-        }
         val notificationID = 5786423// An ID for this notification unique within the app
         // Build a notification with basic info about the song
         val notificationBuilder: Notification.Builder =
@@ -130,19 +91,18 @@ class PlayerService: Service() {
         notificationBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
         notificationBuilder.setContentTitle(title)
         notificationBuilder.setProgress(duration,0,true)
-        notification = notificationBuilder.build() // Requires API level 16
+        val notification = notificationBuilder.build() // Requires API level 16
         startForeground(notificationID, notification)
     }
 
     private fun stop()
     {
         if (isPlaying) {
-            timer?.cancel()
             setIsPlaying(false)
             isPlaying = false
             myPlayer?.release()
             myPlayer = null
-            stopForeground(true)
+            stopForeground(STOP_FOREGROUND_REMOVE)
         }
     }
 
